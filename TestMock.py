9d681c747fe00f4c0f1bfc920222775aaefa4209
Eric A. Evans <eevans@gogoair.com>
2016-03-17 11:54:56 -0600

    Init of Test with Mock

---
+++ TestMock.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+from rmmodule import rm
+
+import mock
+import unittest
+
+class RmTestCase(unittest.TestCase):
+
+    @mock.patch('rmmodule.os')
+    def test_rm(self, mock_os):
+	rm("any path")
+	# Test that rm called os.remove with right parameters
+	mock_os.remove.assert_called_with("any path")
